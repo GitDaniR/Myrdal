a622d5430bc9b00170eb80bc49d057ccbb4dd0ee
Daniel Rachev <d.n.rachev@student.tudelft.nl>
2024-07-20 23:03:34 +0300

    Backend init.

---
+++ backend/myrdal_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyrdalApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myrdal_api'
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// TODO: Edit Default Config
 export default defineConfig({
     plugins: [react()],
     test: {
