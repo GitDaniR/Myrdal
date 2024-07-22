370c449657c7f82f8ee29933ed034e037233ad7f
GitDaniR <dani.rachev@gmail.com>
2024-07-22 20:01:23 +0300

    Added CustomUser model.

--- backend/myrdal_api/models.py
+++ backend/myrdal_api/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+"""Module defining database models."""
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    """ The user model.
+    """
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_of_birth']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
