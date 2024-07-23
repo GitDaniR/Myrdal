6e5ad3a584b4b4c1784636a1204522de30af4e72
GitDaniR <dani.rachev@gmail.com>
2024-07-23 16:17:16 +0300

    Make import verbose.

--- backend/myrdal_api/models.py
+++ backend/myrdal_api/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from .managers import CustomUserManager
+from myrdal_api.managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
