54a859b9faba1f33eb8ea55ebd0b252826d1f800
GitDaniR <dani.rachev@gmail.com>
2024-07-24 23:12:29 +0300

    Added client requests for login and registration.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,27 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
+import https from "https";
 
 // https://vitejs.dev/config/
 // TODO: Edit Default Config
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        coverage: {
-          reporter: ['text', 'json-summary', 'json'],
-          reportOnFailure: true,
-        }
+  plugins: [react(), mkcert()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+        agent: new https.Agent(),
+      },
     },
+  },
+  test: {
+      coverage: {
+        reporter: ['text', 'json-summary', 'json'],
+        reportOnFailure: true,
+      }
+  },
 })
