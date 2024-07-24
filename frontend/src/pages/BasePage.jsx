180167f6e1eda88f92e12b7aec41adc18b10a5c1
GitDaniR <dani.rachev@gmail.com>
2024-07-24 20:37:55 +0300

    Created BasePage component and extracted Login Form.

---
+++ frontend/src/pages/BasePage.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+const BasePage = ({ children }) => {
+    return (
+        <div className="container mx-auto flex justify-between px-36 py-48">
+            <div className='mt-7 space-y-2'>
+                <label className='block text-[#22c55e] text-6xl'>Myrdal</label>
+                <label className="block max-w-80 text-3xl text-wrap">Managing your finances has never been easier.</label>
+            </div>
+            { children }
+        </div>
+    );
+}
+BasePage.propTypes = {
+    children: PropTypes.element.isRequired
+}
+
+export default BasePage
