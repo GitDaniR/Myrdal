0d750cb11e6f85692081d7e8ae9915e1d693c26c
GitDaniR <dani.rachev@gmail.com>
2024-07-24 21:17:40 +0300

    Added styling to registration form.

--- frontend/src/components/RegistrationForm/RegistrationForm.jsx
+++ frontend/src/components/RegistrationForm/RegistrationForm.jsx
@@ -6,31 +6,19 @@ const handleRegisterSubmit = (e) => {
 
 const RegistrationForm = () => {
     return (
-        <div>
-            <form>
-                <div>
-                    <label htmlFor="firstName">First name</label>
-                    <input id="firstName" placeholder="Enter your first name" required></input>
-                </div>
-                <div>
-                    <label htmlFor="lastName">Last name</label>
-                    <input id="lastName" placeholder="Enter your last name" required></input>
-                </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" placeholder="Enter your email" required></input>
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" placeholder="Enter your password" required></input>
-                </div>
-                <div>
-                    <label htmlFor="dateOfBirth">Date of birth</label>
-                    <input type="date" id="dateOfBirth" required></input>
-                </div>
-                <div>
-                    <button type="button" onClick={handleRegisterSubmit}>Sign Up</button>
-                </div>
+        <div className='form-container'>
+            <form className='form-block'>
+                <label htmlFor="email" className="block">Email</label>
+                <input type="email" id="email" placeholder="Enter your email" required  className='input-field'></input>
+                <label htmlFor="password" className="block">Password</label>
+                <input type="password" id="password" placeholder="Enter your password" required  className='input-field'></input>
+                <label htmlFor="firstName">First name</label>
+                <input id="firstName" placeholder="Enter your first name" required className='input-field'></input>
+                <label htmlFor="lastName">Last name</label>
+                <input id="lastName" placeholder="Enter your last name" required className='input-field'></input>
+                <label htmlFor="dateOfBirth">Date of birth</label>
+                <input type="date" id="dateOfBirth" required className="input-field"></input>
+                <button type="button" onClick={handleRegisterSubmit} className="form-btn">Sign Up</button>
             </form>
         </div>
     )
