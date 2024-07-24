70e727d75493e6acbad88e99768cbdc2d8f88d24
GitDaniR <dani.rachev@gmail.com>
2024-07-24 21:08:52 +0300

    Extracted form styling.

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -8,14 +8,14 @@ const handleLoginSubmit = (e) => {
 
 const LoginForm = () => {
     return (
-        <div className='shadow-lg rounded-lg bg-white text-xl p-12'>
-            <form className='space-y-1.5'>
-                <label htmlFor="email" className="block">Email</label>
-                <input type="email" id="email" placeholder="Enter your email" required className="block border-2 rounded-lg p-1"></input>
-                <label htmlFor="password" className="block">Password</label>
-                <input type="password" id="password" placeholder="Enter your password" required className="block border-2 rounded-lg p-1"></input>
-                <Link to="/register" className="block text-sm text-center underline text-blue-600 hover:text-blue-800">Create new account</Link>
-                <button type="button" onClick={handleLoginSubmit} className="block border-2 rounded-lg mx-auto px-12 py-1 bg-[#22c55e] hover:bg-[#15803d] delay-50 duration-300 ease-in-out text-white">Log In</button>
+        <div className='form-container'>
+            <form className='form-block'>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" placeholder="Enter your email" required className="input-field"></input>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" placeholder="Enter your password" required className="input-field"></input>
+                <Link to="/register" className="hyperlink">Create new account</Link>
+                <button type="button" onClick={handleLoginSubmit} className="form-btn">Log In</button>
             </form>
         </div>
     );
