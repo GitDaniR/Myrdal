b6bcb6551484aacc090b272b74787a0a58821f16
GitDaniR <dani.rachev@gmail.com>
2024-07-27 23:04:43 +0300

    Added ToastList component.

---
+++ frontend/src/components/ToastList/ToastList.jsx
@@ -0,0 +1,23 @@
+import Toast from "../Toast";
+import PropTypes from 'prop-types';
+
+const ToastList = ({ data, removeToast }) => {
+    return data.length > 0 && (
+        <div className="fixed top-0.5 right-0.5 w-80 space-y-0.5">
+            {data.map((toast) => (
+                <Toast 
+                    key={toast.id} 
+                    type={toast.type} 
+                    message={toast.message} 
+                    closeToast={() => removeToast(toast.id)}
+                />
+            ))}
+        </div>
+    );
+}
+ToastList.propTypes = {
+    data: PropTypes.array,
+    removeToast: PropTypes.func.isRequired,
+}
+
+export default ToastList
