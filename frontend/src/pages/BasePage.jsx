ca16c733b8743475c3309f0a12880d1c2496f324
GitDaniR <dani.rachev@gmail.com>
2024-07-27 17:20:37 +0300

    Replaced <label> with <p>.

--- frontend/src/pages/BasePage.jsx
+++ frontend/src/pages/BasePage.jsx
@@ -4,8 +4,8 @@ const BasePage = ({ children }) => {
     return (
         <div className="container mx-auto flex justify-between px-36 py-32">
             <div className='mt-7 space-y-2'>
-                <label className='block text-[#22c55e] text-6xl'>Myrdal</label>
-                <label className="block max-w-80 text-3xl text-wrap">Managing your finances has never been easier.</label>
+                <p className='block text-[#22c55e] text-6xl'>Myrdal</p>
+                <p className="block max-w-80 text-3xl text-wrap">Managing your finances has never been easier.</p>
             </div>
             { children }
         </div>
