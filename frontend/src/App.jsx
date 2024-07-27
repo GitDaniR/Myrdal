60fa1a2ed4d914c74d9039f8ff150bfcdc807551
GitDaniR <dani.rachev@gmail.com>
2024-07-27 23:05:35 +0300

    Added ToastContext so that any component can create toasts.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
 import './App.css'
+import { useState } from "react";
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import LoginForm from './components/LoginForm'
 import RegistrationForm from './components/RegistrationForm'
 import BasePage from './pages/BasePage'
+import ToastList from './components/ToastList'
+import ToastContext from './contexts/ToastContext'
 
 function App() {
+  const [toasts, setToasts] = useState([]);
+
+  const showToast = (type, message) => {
+    const toast = {
+      'id': Date.now(),
+      'type': type,
+      'message': message,
+    }
+
+    setTimeout(() => {
+      removeToast(toast.id)
+    }, 100000); // 10 seconds
+
+    setToasts((prevToasts) => [...prevToasts, toast]);
+  }
+
+  const removeToast = (id) => {
+    setToasts((prevToasts) => prevToasts.filter(toast => toast.id !== id) )
+  }
 
   const router = createBrowserRouter(createRoutesFromElements(
     <>
@@ -14,8 +36,11 @@ function App() {
   ))
 
   return (
-    <RouterProvider router={router} />
-  )
+      <ToastContext.Provider value={showToast}>
+      <RouterProvider router={router} />
+      <ToastList data={toasts} removeToast={removeToast}/>
+      </ToastContext.Provider>
+  );
 }
 
 export default App
