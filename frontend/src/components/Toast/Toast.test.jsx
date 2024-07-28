e1b20fbc59a394ad4e0d48a5721c1e43e42d6a0f
GitDaniR <dani.rachev@gmail.com>
2024-07-28 22:33:16 +0300

    Added tests for ToastList component.

--- frontend/src/components/Toast/Toast.test.jsx
+++ frontend/src/components/Toast/Toast.test.jsx
@@ -6,32 +6,38 @@ import Toast from './Toast';
 describe('Toast Component', () => {
     it('renders success toast with correct icon and message', () => {
         render(<Toast type="success" message="Success!" closeToast={() => {}} />);
+
         expect(screen.getByText('Success!')).toBeInTheDocument();
         expect(screen.getByTestId('success-icon')).toBeInTheDocument();
     });
 
     it('renders info toast with correct icon and message', () => {
         render(<Toast type="info" message="Information!" closeToast={() => {}} />);
+
         expect(screen.getByText('Information!')).toBeInTheDocument();
         expect(screen.getByTestId('info-icon')).toBeInTheDocument();
     });
 
     it('renders error toast with correct icon and message', () => {
         render(<Toast type="error" message="Error!" closeToast={() => {}} />);
+
         expect(screen.getByText('Error!')).toBeInTheDocument();
         expect(screen.getByTestId('error-icon')).toBeInTheDocument();
     });
 
     it('renders toast with no icon and correct message', () => {
         render(<Toast type="not-an-icon" message="No icon!" closeToast={() => {}} />);
+
         expect(screen.getByText('No icon!')).toBeInTheDocument();
         expect(document.getElementsByTagName('svg').length).toBe(1);
     });
 
     it('calls closeToast function when close button is clicked', () => {
         const closeToastMock = vi.fn();
+
         render(<Toast type="success" message="Success!" closeToast={closeToastMock} />);
         fireEvent.click(screen.getByRole('button'));
+        
         expect(closeToastMock).toHaveBeenCalledTimes(1);
     });
 });
--- frontend/src/components/ToastList/ToastList.jsx
+++ frontend/src/components/ToastList/ToastList.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const ToastList = ({ data, removeToast }) => {
     return data.length > 0 && (
-        <div className="fixed top-0.5 right-0.5 w-80 space-y-0.5">
+        <div className="fixed top-0.5 right-0.5 w-80 space-y-0.5" data-testid="toast-list">
             {data.map((toast) => (
                 <Toast 
                     key={toast.id} 
