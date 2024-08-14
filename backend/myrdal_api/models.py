ffb842f67095980874b7bc904719d54ebe0c3f42
GitDaniR <dani.rachev@gmail.com>
2024-08-14 20:55:38 +0300

    Improved documentation for CustomUser model.

--- backend/myrdal_api/models.py
+++ backend/myrdal_api/models.py
@@ -8,16 +8,25 @@ from myrdal_api.managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
-    """The user model."""
+    """Custom User model that represents a user in the system.
+
+    This model extends Django's built-in AbstractUser model and overrides the username
+    field with email to use email as the primary identifier. It also adds a date_of_birth field.
+    """
 
     username = None
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField(_("email address"), unique=True)
     date_of_birth = models.DateField()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_of_birth']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "date_of_birth"]
 
     objects = CustomUserManager()
 
     def __str__(self):
+        """ Returns a string representation of the CustomUser instance.
+
+        Returns:
+            str: The email of the user.
+        """        
         return self.email
