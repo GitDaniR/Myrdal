d94c7d446a1c0c3377a1101944a8dee11c0aa5c8
GitDaniR <dani.rachev@gmail.com>
2024-08-15 12:39:25 +0300

    Added Transaction model.

--- backend/myrdal_api/models.py
+++ backend/myrdal_api/models.py
@@ -8,7 +8,8 @@ from myrdal_api.managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
-    """Custom User model that represents a user in the system.
+    """
+    Custom User model that represents a user in the system.
 
     This model extends Django's built-in AbstractUser model and overrides the username
     field with email to use email as the primary identifier. It also adds a date_of_birth field.
@@ -24,12 +25,14 @@ class CustomUser(AbstractUser):
     objects = CustomUserManager()
 
     def __str__(self):
-        """ Returns a string representation of the CustomUser instance.
+        """
+        Returns a string representation of the CustomUser instance.
 
         Returns:
             str: The email of the user.
-        """        
+        """
         return self.email
+
 
 class Account(models.Model):
     """
@@ -50,3 +53,42 @@ class Account(models.Model):
             str: The name of the account.
         """
         return self.account_name
+
+
+class Transaction(models.Model):
+    """
+    Represents a financial transaction for a user's account.
+    It stores the name of the payee, the date and time of the transaction,
+    the amount, a description, and a category.
+
+    Note:
+        The description and the category are optional.
+
+    """    
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    payee = models.CharField(max_length=100)
+    date_time = models.DateTimeField()
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    description = models.TextField(max_length=500, blank=True)
+    CATEGORY_CHOICES = [
+        ("food", "Food"),
+        ("shopping", "Shopping"),
+        ("travel", "Travel"),
+        ("healthcare", "Healthcare"),
+        ("entertainment", "Entertainment"),
+        ("utilities", "Utilities"),
+        ("education", "Education"),
+        ("gifts_donations", "Gifts/Donations"),
+        ("unspecified", ""),
+    ]
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Account instance.
+
+        Returns:
+            str: The payee, the amount, the date-time and the category.
+        """        
+        formatted_date_time = self.date_time.strftime("%Y-%m-%d %H:%M:%S")
+        return f"{self.payee} : {self.amount:.2f} , {formatted_date_time} , ({self.category})"
