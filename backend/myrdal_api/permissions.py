0391afe3feab8c3071ae843a8fcd439e92d3035c
GitDaniR <dani.rachev@gmail.com>
2024-08-15 21:41:36 +0300

    Added API view for transactions - retrieve all transactions for user, create, update, delete.

--- backend/myrdal_api/permissions.py
+++ backend/myrdal_api/permissions.py
@@ -1,12 +1,16 @@
 from rest_framework import permissions
 
+from myrdal_api.models import Account, Transaction
+
+
 class IsOwner(permissions.BasePermission):
     """
     A custom permission class that checks if the current user is the owner of the object.
 
     Inherits:
         permissions.BasePermission: Abstract base class for all permission classes.
-    """    
+    """
+
     def has_object_permission(self, request, view, obj):
         """
         Overridden method from BasePermission to check if the request user is the owner of the object.
@@ -18,5 +22,9 @@ class IsOwner(permissions.BasePermission):
 
         Returns:
             bool: True if the request user is the owner of the object, False otherwise.
-        """        
-        return obj.user == request.user
+        """
+        if isinstance(obj, Account):
+            return obj.user == request.user
+        elif isinstance(obj, Transaction):
+            return obj.account.user == request.user
+        return False
--- backend/myrdal_api/urls.py
+++ backend/myrdal_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from myrdal_api.views import AccountApiView
+from myrdal_api.views import AccountApiView, TransactionApiView
 
 urlpatterns = [
     path('accounts/', AccountApiView.as_view(), name='account_list_create'),
     path('accounts/<int:account_id>/', AccountApiView.as_view(), name='account_detail'),
+    path('transactions/', TransactionApiView.as_view(), name='transaction_list_create'),
+    path('transactions/<int:transaction_id>/', TransactionApiView.as_view(), name='transaction_detail'),
 ]
