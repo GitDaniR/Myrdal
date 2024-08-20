f0f7bda43080f53a17a873f17a39327d0292ac17
GitDaniR <dani.rachev@gmail.com>
2024-08-21 01:55:56 +0300

    Updated configuration for backend testing workflow.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,6 +129,9 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
+        'TEST': {
+            'NAME': os.environ.get("DB_NAME"),
+        },
     }
 }
 
