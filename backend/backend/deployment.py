7bda120c9ecc95244d9188e20ca2d35ff2a5dafe
GitDaniR <dani.rachev@gmail.com>
2024-08-21 14:48:21 +0300

    Removed loaddotenv() and CORS_ORIGINS_WHITELIST.

--- backend/backend/deployment.py
+++ backend/backend/deployment.py
@@ -3,28 +3,23 @@ Django settings for deployment of the backend project.
 """
 
 import os
-from dotenv import load_dotenv
 
 from .settings import *  # pylint: disable=wildcard-import, unused-wildcard-import
 from .settings import BASE_DIR
 
-# Load environment variables
-load_dotenv()
-
 # Secret key
-SECRET_KEY = os.environ.get("API_SECRET_KEY")
+SECRET_KEY = os.environ["API_SECRET_KEY"]
 
 # Disable debug mode
 DEBUG = False
 
 # Backend
-ALLOWED_HOSTS = [os.environ.get("WEBSITE_HOSTNAME")]
-CSRF_TRUSTED_ORIGINS = ["https://" + os.environ.get("WEBSITE_HOSTNAME")]
+ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
+CSRF_TRUSTED_ORIGINS = ["https://" + os.environ["WEBSITE_HOSTNAME"]]
 APPEND_SLASH = True
 
 # Frontend
 CORS_ALLOWED_ORIGINS = ["https://gitdanir.github.io/Myrdal"]
-CORS_ORIGIN_WHITELIST = []
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
@@ -34,7 +29,7 @@ REST_FRAMEWORK = {
 
 
 # Database configuration
-CONNECTION = os.environ.get("AZURE_POSTGRESQL_CONNECTIONSTRING")
+CONNECTION = os.environ["AZURE_POSTGRESQL_CONNECTIONSTRING"]
 CONNECTION_STR = {
     pair.split("=")[0]: pair.split("=")[1] for pair in CONNECTION.split(" ")
 }
