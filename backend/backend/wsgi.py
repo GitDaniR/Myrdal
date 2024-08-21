14e3150344d162ca18d24285fa10681a29b75865
GitDaniR <dani.rachev@gmail.com>
2024-08-21 07:17:37 +0300

    Configured frontend deployment fully and part of backend.

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+SETTINGS_MODULE = (
+    "backend.deployment" if "WEBSITE_HOSTNAME" in os.environ else "backend.settings"
+)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", SETTINGS_MODULE)
 
 application = get_wsgi_application()
